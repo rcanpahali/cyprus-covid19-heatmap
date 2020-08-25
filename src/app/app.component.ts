92a1cec8c1c79fe72c67d9ec385a5c1db2d6976c
rcanpahali <rcanpahali@gmail.com>
2020-08-25 14:05:43 +0300

    assets updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,39 @@
 import { Component } from "@angular/core";
 import * as L from "leaflet";
-import 'leaflet.heat/dist/leaflet-heat.js'
-import { addressPoints } from './../assets/realworld.10000'
+import "leaflet.heat/dist/leaflet-heat.js";
+import { addressPoints } from "./../assets/realworld.10000";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   options = {
     layers: [
       L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 18,
-        attribution: ""
-      })
+        maxZoom: 17,
+        minZoom: 10,
+        attribution: "R. Can PAHALI",
+        opacity: 1,
+      }),
     ],
-    zoom: 12,
-    center: L.latLng(-37.87, 175.475)
+    zoom: 13,
+    center: L.latLng(35.175921, 33.364245),
   };
 
   onMapReady(map) {
-    let newAddressPoints = addressPoints.map(function (p) { return [p[0], p[1]]; });
-    const heat = (L as any).heatLayer(newAddressPoints).addTo(map);
+    let heatLayerOptions = {
+      minOpacity: 0.80,
+      radius: 60,
+      blur: 25,
+      //gradient: {0.4: 'blue', 0.65: 'lime', 1: 'red'}
+    };    
+    let newAddressPoints = addressPoints.map(function (p) {
+      return [p[0], p[1]];
+    });
+    const heat = (L as any)
+      .heatLayer(newAddressPoints, heatLayerOptions)
+      .addTo(map);
   }
 }
